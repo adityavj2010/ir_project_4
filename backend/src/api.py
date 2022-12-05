840d6075e296961cb5fd4d5723cf56e375460468
ashishp911 <ashish.patil35@gmail.com>
2022-12-04 20:19:51 -0500

    getting predictions euu

--- backend/src/api.py
+++ backend/src/api.py
@@ -8,14 +8,15 @@ api = Blueprint('api', __name__)
 solr = Solr()
 def getClassifiedLabel():
     import random
-    return random.choice([0,1])
+    # return random.choice([0,1])
+    return 0
 
 @api.route('/query',methods=['POST'])
 def query():
     body = request.json
     text = body["query"]
     topic = body["topic"]
-    if getClassifiedLabel==0:
+    if getClassifiedLabel() == 0:
         result = solr.chitchat_query(text,topic)
     else:
         result = solr.reddit_query(text, topic)
