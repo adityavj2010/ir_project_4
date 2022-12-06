8d9c8fbf7fa5746159b8df734a673927778b76e9
ashishp911 <ashish.patil35@gmail.com>
2022-12-06 17:18:46 -0500

    reddit index also working now

--- backend/src/solr.py
+++ backend/src/solr.py
@@ -2,15 +2,14 @@ import pysolr
 import requests
 import urllib.parse
 from env import URL
-import urllib.parse
-# from '/backend/classifier/classifier.py' import clas
 
 
 def make_response(res):
     if res.ok:
         response = res.json()['response']
         if response['numFound'] > 0:
-            if response['docs'][0]['score'] > 10:
+
+            # if response['docs'][0]['score'] > 10:
                 return {
                     "message": response['docs'][0]['response'][0]
                 }
@@ -19,14 +18,14 @@ def make_response(res):
     }
 
 def make_query(query):
-    return "/select?fl=score%2Cresponse&indent=true&q.op=OR&q=" + \
-            urllib.parse(query)+'&rows=1&wt=json'
+    return "/select?fl=score%2Cresponse&indent=true&q.op=OR&q=" + urllib.parse.quote(query)+'&rows=1&wt=json'
 
 
 class Solr:
 
     def reddit_query(self, text, topic=None):
         query = f'(query:"{text}"~30)^2 or response:"{text}"~30'
+        # print(query)
         if topic:
             query += " topic:"+topic
         reddit_query = make_query(query)
@@ -34,7 +33,11 @@ class Solr:
         return make_response(res)
 
     def chitchat_query(self, text, topic):
+        # text = f'"text'
         query = f'(query:"{text}"~30)^2 or response:"{text}"~30'
+        '''
+        http://34.130.165.83:8983/solr/chitchat/select?indent=true&q.op=OR&q=query%3A%22Hello%22
+        '''
         chitchat_query = make_query(query)
         res = requests.get(URL["chitchat"]+chitchat_query)
         return make_response(res)
